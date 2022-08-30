9eebf44e0a145c53d9e0951e530ffd429555afc1
satriaprayoga <satria.prayoga@gmail.com>
2022-08-30 16:13:41 +0700

    error

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,28 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, CSSProperties } from 'react'
 import { render } from 'react-dom';
+import ClipLoader from "react-spinners/ClipLoader";
+import { PulseLoader } from 'react-spinners';
 import './assets/popup.css'
+import skpd from './skpd';
+
+const override
+ = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
 
 const Popup = () => {
 
   const [token,setToken]=useState('');
   const [expired,setExpired]=useState('');
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(false);
+  const [errorMsg,setErrorMsg]=useState('');
 
   const authenticate = async () => {
+    setLoading(true);
     try {
       const loginFormData = new FormData();
       loginFormData.append("username", "kabbogor")
@@ -18,8 +32,12 @@ const Popup = () => {
       setToken(response.data.token);
       setExpired(response.data.expire);
     } catch (error) {
-
+      console.log(error.code);
+      console.log(error.message);
+      setError(true);
+      setErrorMsg(error.message);
     }
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -27,32 +45,35 @@ const Popup = () => {
   }, [])
 
   return (
-    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <div>
-          <img class="mx-auto h-20 w-auto" src="http://diskominfo.bogorkab.go.id/wp-content/uploads/2017/02/cropped-logo-pemkab-bogor.png" alt="Workflow" />
-          <h2 class="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Cari BAST</h2>
-          <p class="mt-2 text-center text-sm text-gray-600">
-            Input Parameter Pencarian
-            {expired}
-          </p>
+          <img className="mx-auto h-20 w-auto" src="http://diskominfo.bogorkab.go.id/wp-content/uploads/2017/02/cropped-logo-pemkab-bogor.png" alt="Workflow" />
+          <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Cari BAST</h2>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        
+        <div className='flex items-center justify-center'>
+          <PulseLoader color='#ccc' loading={loading} cssOverride={override} size={20} />
+        </div>
+        {
+          (!loading && !error)?
+          <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={(e)=>{e.preventDefault();setLoading(!loading)}}>
           <input type="hidden" name="remember" value="true" />
-          <div class="rounded-md shadow-sm -space-y-px">
+          <div className="rounded-md shadow-sm -space-y-px">
             {/* <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
+            <label for="email-address" className="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
           </div>
           <div>
-            <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
+            <label for="password" className="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
           </div> */}
             <div>
-              <label for="kode_skpd" class="sr-only">SKPD</label>
-              <select id="kode_skpd" name='kode_skpd' required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
-                <option value='1.0.1.1'>DINAS PENDIDIKAN</option>
-                <option value='1.0.1.2'>DINAS KESEHATAN</option>
+              <label for="kode_skpd" className="sr-only">SKPD</label>
+              <select id="kode_skpd" name='kode_skpd' required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                {skpd.map((s)=>(
+                  <option value={s.kode} key={s.kode}>{s.nama_skpd}</option>
+                ))}
               </select>
 
             </div>
@@ -61,12 +82,21 @@ const Popup = () => {
 
 
           <div>
-            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 
-              CARI
+              Tampilkan BAST
             </button>
+           
           </div>
         </form>
+        :
+        <div>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            {errorMsg}
+          </p>
+        </div>
+        }
+        
       </div>
     </div>
   )
---
+++ src/skpd.js
@@ -0,0 +1,292 @@
+const skpd=[
+    {
+      "kode": "1.01.01",
+      "nama_skpd": "DINAS PENDIDIKAN"
+    },
+    {
+      "kode": "1.02.01",
+      "nama_skpd": "DINAS KESEHATAN"
+    },
+    {
+      "kode": "1.03.01",
+      "nama_skpd": "DINAS PEKERJAAN UMUM DAN PENATAAN RUANG"
+    },
+    {
+      "kode": "1.04.01",
+      "nama_skpd": "DINAS PERUMAHAN, KAWASAN PERMUKIMAN DAN PERTANAHAN"
+    },
+    {
+      "kode": "1.05.01",
+      "nama_skpd": "SATUAN POLISI PAMONG PRAJA"
+    },
+    {
+      "kode": "1.05.02",
+      "nama_skpd": "BADAN PENANGGULANGAN BENCANA DAERAH"
+    },
+    {
+      "kode": "1.05.03",
+      "nama_skpd": "DINAS PEMADAM KEBAKARAN"
+    },
+    {
+      "kode": "1.06.01",
+      "nama_skpd": "DINAS SOSIAL"
+    },
+    {
+      "kode": "2.07.01",
+      "nama_skpd": "DINAS TENAGA KERJA"
+    },
+    {
+      "kode": "2.08.01",
+      "nama_skpd": "DINAS PEMBERDAYAAN PEREMPUAN DAN PERLINDUNGAN ANAK, PENGENDALIAN PENDUDUK DAN KELUARGA BERENCANA"
+    },
+    {
+      "kode": "2.09.01",
+      "nama_skpd": "DINAS KETAHANAN PANGAN"
+    },
+    {
+      "kode": "2.11.01",
+      "nama_skpd": "DINAS LINGKUNGAN HIDUP"
+    },
+    {
+      "kode": "2.12.01",
+      "nama_skpd": "DINAS KEPENDUDUKAN DAN PENCATATAN SIPIL"
+    },
+    {
+      "kode": "2.13.01",
+      "nama_skpd": "DINAS PEMBERDAYAAN MASYARAKAT DAN DESA"
+    },
+    {
+      "kode": "2.15.01",
+      "nama_skpd": "DINAS PERHUBUNGAN"
+    },
+    {
+      "kode": "2.16.01",
+      "nama_skpd": "DINAS KOMUNIKASI DAN INFORMATIKA"
+    },
+    {
+      "kode": "2.17.01",
+      "nama_skpd": "DINAS KOPERASI, USAHA KECIL DAN MENENGAH"
+    },
+    {
+      "kode": "2.18.01",
+      "nama_skpd": "DINAS PENANAMAN MODAL DAN PELAYANAN TERPADU SATU PINTU"
+    },
+    {
+      "kode": "2.19.01",
+      "nama_skpd": "DINAS PEMUDA DAN OLAHRAGA"
+    },
+    {
+      "kode": "2.22.01",
+      "nama_skpd": "DINAS KEBUDAYAAN DAN PARIWISATA"
+    },
+    {
+      "kode": "2.23.01",
+      "nama_skpd": "DINAS ARSIP DAN PERPUSTAKAAN"
+    },
+    {
+      "kode": "3.25.01",
+      "nama_skpd": "DINAS PERIKANAN DAN PETERNAKAN"
+    },
+    {
+      "kode": "3.27.01",
+      "nama_skpd": "DINAS TANAMAN PANGAN, HORTIKULTURA DAN PERKEBUNAN"
+    },
+    {
+      "kode": "3.30.01",
+      "nama_skpd": "DINAS PERDAGANGAN DAN PERINDUSTRIAN"
+    },
+    {
+      "kode": "4.01.01",
+      "nama_skpd": "SEKRETARIAT DAERAH"
+    },
+    {
+      "kode": "4.02.01",
+      "nama_skpd": "SEKRETARIAT DEWAN PERWAKILAN RAKYAT DAERAH"
+    },
+    {
+      "kode": "5.01.01",
+      "nama_skpd": "BADAN PERENCANAAN PEMBANGUNAN, PENELITIAN DAN PENGEMBANGAN DAERAH"
+    },
+    {
+      "kode": "5.02.01",
+      "nama_skpd": "BADAN PENGELOLAAN KEUANGAN DAN ASET DAERAH"
+    },
+    {
+      "kode": "5.02.02",
+      "nama_skpd": "BADAN PENGELOLAAN PENDAPATAN DAERAH"
+    },
+    {
+      "kode": "5.03.01",
+      "nama_skpd": "BADAN KEPEGAWAIAN, PENDIDIKAN DAN PELATIHAN"
+    },
+    {
+      "kode": "6.01.01",
+      "nama_skpd": "INSPEKTORAT"
+    },
+    {
+      "kode": "7.01.01",
+      "nama_skpd": "KECAMATAN CIBINONG"
+    },
+    {
+      "kode": "7.01.02",
+      "nama_skpd": "KECAMATAN GUNUNG PUTRI"
+    },
+    {
+      "kode": "7.01.03",
+      "nama_skpd": "KECAMATAN CITEUREUP"
+    },
+    {
+      "kode": "7.01.04",
+      "nama_skpd": "KECAMATAN SUKARAJA"
+    },
+    {
+      "kode": "7.01.05",
+      "nama_skpd": "KECAMATAN BABAKAN MADANG"
+    },
+    {
+      "kode": "7.01.06",
+      "nama_skpd": "KECAMATAN JONGGOL"
+    },
+    {
+      "kode": "7.01.07",
+      "nama_skpd": "KECAMATAN CILEUNGSI"
+    },
+    {
+      "kode": "7.01.08",
+      "nama_skpd": "KECAMATAN CARIU"
+    },
+    {
+      "kode": "7.01.09",
+      "nama_skpd": "KECAMATAN SUKAMAKMUR"
+    },
+    {
+      "kode": "7.01.10",
+      "nama_skpd": "KECAMATAN PARUNG"
+    },
+    {
+      "kode": "7.01.11",
+      "nama_skpd": "KECAMATAN GUNUNG SINDUR"
+    },
+    {
+      "kode": "7.01.12",
+      "nama_skpd": "KECAMATAN KEMANG"
+    },
+    {
+      "kode": "7.01.13",
+      "nama_skpd": "KECAMATAN BOJONG GEDE"
+    },
+    {
+      "kode": "7.01.14",
+      "nama_skpd": "KECAMATAN LEUWILIANG"
+    },
+    {
+      "kode": "7.01.15",
+      "nama_skpd": "KECAMATAN  CIAMPEA"
+    },
+    {
+      "kode": "7.01.16",
+      "nama_skpd": "KECAMATAN CIBUNGBULANG"
+    },
+    {
+      "kode": "7.01.17",
+      "nama_skpd": "KECAMATAN PAMIJAHAN"
+    },
+    {
+      "kode": "7.01.18",
+      "nama_skpd": "KECAMATAN RUMPIN"
+    },
+    {
+      "kode": "7.01.19",
+      "nama_skpd": "KECAMATAN JASINGA"
+    },
+    {
+      "kode": "7.01.20",
+      "nama_skpd": "KECAMATAN PARUNG PANJANG"
+    },
+    {
+      "kode": "7.01.21",
+      "nama_skpd": "KECAMATAN NANGGUNG"
+    },
+    {
+      "kode": "7.01.22",
+      "nama_skpd": "KECAMATAN CIGUDEG"
+    },
+    {
+      "kode": "7.01.23",
+      "nama_skpd": "KECAMATAN TENJO"
+    },
+    {
+      "kode": "7.01.24",
+      "nama_skpd": "KECAMATAN CIAWI"
+    },
+    {
+      "kode": "7.01.25",
+      "nama_skpd": "KECAMATAN CISARUA"
+    },
+    {
+      "kode": "7.01.26",
+      "nama_skpd": "KECAMATAN MEGAMENDUNG"
+    },
+    {
+      "kode": "7.01.27",
+      "nama_skpd": "KECAMATAN CARINGIN"
+    },
+    {
+      "kode": "7.01.28",
+      "nama_skpd": "KECAMATAN CIJERUK"
+    },
+    {
+      "kode": "7.01.29",
+      "nama_skpd": "KECAMATAN CIOMAS"
+    },
+    {
+      "kode": "7.01.30",
+      "nama_skpd": "KECAMATAN DRAMAGA"
+    },
+    {
+      "kode": "7.01.31",
+      "nama_skpd": "KECAMATAN TAMANSARI"
+    },
+    {
+      "kode": "7.01.32",
+      "nama_skpd": "KECAMATAN KLAPA NUNGGAL"
+    },
+    {
+      "kode": "7.01.33",
+      "nama_skpd": "KECAMATAN CISEENG"
+    },
+    {
+      "kode": "7.01.34",
+      "nama_skpd": "KECAMATAN RANCA BUNGUR"
+    },
+    {
+      "kode": "7.01.35",
+      "nama_skpd": "KECAMATAN SUKAJAYA"
+    },
+    {
+      "kode": "7.01.36",
+      "nama_skpd": "KECAMATAN TANJUNGSARI"
+    },
+    {
+      "kode": "7.01.37",
+      "nama_skpd": "KECAMATAN TAJURHALANG"
+    },
+    {
+      "kode": "7.01.38",
+      "nama_skpd": "KECAMATAN CIGOMBONG"
+    },
+    {
+      "kode": "7.01.39",
+      "nama_skpd": "KECAMATAN LEUWISADENG"
+    },
+    {
+      "kode": "7.01.40",
+      "nama_skpd": "KECAMATAN TENJOLAYA"
+    },
+    {
+      "kode": "8.01.01",
+      "nama_skpd": "BADAN KESATUAN BANGSA DAN POLITIK"
+    }
+  ];
+
+export default skpd;
